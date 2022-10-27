22d24babcff67e4fdfa6a72f6a99d01435a10818
Flavia <flavia.lima643@gmail.com>
2022-10-27 11:33:09 -0300

    Adicionando Before

--- src/test/java/gamemania/IndexTest.java
+++ src/test/java/gamemania/IndexTest.java
@@ -1,5 +1,6 @@
 package gamemania;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,12 +10,16 @@ public class IndexTest {
 	ChromeDriver driver;// Driver do Google Chrome
 
 	// Pré teste
-	@Test
+	@Before
 	public void PreTeste() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\chromedriver\\chromedriver.exe");
-
 		driver = new ChromeDriver();// Abrir o navegador
 		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+public void testeNavegador() {
 		driver.get("https://flavia964.github.io/senai-projeto-gameMania/");
 		driver.findElement(By.id("txtBusca")).sendKeys("Jogos");
 		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys(Keys.ENTER);
